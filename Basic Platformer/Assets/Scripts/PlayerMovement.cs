69e7fbb5d56ce875485fe69156009ff9823fd4eb
Arman Kumar Aditya <44armaan@gmail.com>
2023-05-05 18:44:00 +0530

    Animations and movement

---
+++ Basic Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb; 
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private float dirX;
+    [SerializeField] private float movespeed = 7f;
+    [SerializeField] private float jumpforce = 14f;
+
+    private enum MovementState { idle, running, jumping, falling }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+       anim = GetComponent<Animator>();
+       sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * movespeed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+        }
+        
+        UpdateAnimationState();
+    }
+    private void UpdateAnimationState()
+    {
+        MovementState state;
+        
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else 
+        {
+            state = MovementState.idle;
+        }
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+    }
+}
